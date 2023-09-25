2ecfe98dc6ee7e77e964c741c7ad814cb322ff89
Malay <108186647+Malay207@users.noreply.github.com>
2023-09-25 21:06:20 +0530

    authentication added

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const jwt_secrete = process.env.WEBTOKEN;
 const fetchuser = async (req, res, next) => {
-    const token = req.header("auth-token");
+    const token = req.header("auth-Token");
     if (!token) {
         res.status(401).send({ error: "Please authenticate using a valid token" });
     }
